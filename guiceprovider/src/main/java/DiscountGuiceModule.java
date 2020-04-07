dbce7823004df65e0b1b82c3b8b8b8ecce7b43e8
Murphy Liu <murphykobe@gmail.com>
2020-04-07 12:56:22 -0700

    #Google Guice MapBinder

--- guiceprovider/src/main/java/DiscountGuiceModule.java
+++ guiceprovider/src/main/java/DiscountGuiceModule.java
@@ -1,7 +1,33 @@
+import java.time.LocalTime;
+import com.google.inject.Provides;
+
 public class DiscountGuiceModule extends com.google.inject.AbstractModule {
 
     @Override
     protected void configure(){
-       bind(Discountable.class).toProvider(DiscountProvider.class);
+       //bind(Discountable.class).toProvider(DiscountProvider.class);
     }
+
+
+//    @Provides
+//    public Discountable get(){
+//        int hour = LocalTime.now().getHour();
+//
+//        if(isEarlyMorning((hour))){
+//            return new EarlyBirdDiscount();
+//        }
+//        if(isLateNight(hour)){
+//            return new NightOwlDisocunt();
+//        }
+//        return new NoDiscount();
+//
+//    }
+//    private  boolean isEarlyMorning(int currHour){
+//        return (currHour >= 5 && currHour <= 9 );
+//    }
+//
+//    private  boolean isLateNight(int currHour){
+//        return (currHour >=0 && currHour <=4 );
+//    }
+
 }
--- guiceprovider/src/main/java/Discountable.java
+++ guiceprovider/src/main/java/Discountable.java
@@ -1,6 +1,7 @@
 import com.google.inject.ImplementedBy;
+import com.google.inject.ProvidedBy;
 
-@ImplementedBy(EarlyBirdDiscount.class)
+@ProvidedBy(DiscountProvider.class)
 public interface Discountable {
     double getDiscount();
 }
